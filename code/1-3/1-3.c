42df9806322a94e3b33a8fc4f2ba448b691f985d
박채영 <packcy030411@naver.com>
2026-03-27 10:06:15 +0900

    Update 1-3.c

---
+++ code/1-3/1-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "util.h"
+
+extern void task2();
+extern void logger_global(char*message);
+
+void task1() {
+  for(int i=0; i<3; i++) {
+    logger_static_inline("task3");
+    logger_global("task3");
+  }
+}
+
+int main() {
+  task1();
+  task2():
+  task3();
+  return 0;
+}
