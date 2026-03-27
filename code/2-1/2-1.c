5ff62f9f18191431f73076844a2d7610b1fc51ed
박채영 <packcy030411@naver.com>
2026-03-27 10:16:56 +0900

    Update 2-1.c

---
+++ code/2-1/2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  const char* date = "2026-03-24";
+  char buffer[64];
+  int year, month,day;
+
+  sscanf(date,
+  sprintf(buffer,
+
+  printf("%s\n",buffer);
+  return 0;
+}
